47a967ee66adce552f7965236ab2375d181d4faa
hyperhkm <lucasjamaico@gmail.com>
2023-06-07 11:35:29 -0300

    .

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 'use strict'
 const shapeSelection = ['rock', 'paper', 'scissor'];
-const computerSelection = getComputerChoice(shapeSelection);
 let playerSelection;
 function getComputerChoice (shapeSelection) {
     return shapeSelection [Math.floor(Math.random()*shapeSelection.length)];
 }
 function game(){
-    function playround (playerSelection, computerSelection) {  
-        for (let playerCount = 0, computerCount = 0; playerCount < 5 || computerCount < 5;){  
+    for (let playerCount = 0, computerCount = 0; playerCount < 5 && computerCount < 5;){  
+        const computerSelection = getComputerChoice(shapeSelection);
+        function playround (playerSelection, computerSelection) {  
             playerSelection = prompt ("Rock, Paper, Scissors: Choose your Weapon", );
             playerSelection = playerSelection.toLowerCase();
             computerSelection = computerSelection.toLowerCase();
@@ -17,39 +17,39 @@ function game(){
             else if (playerSelection == 'paper') {
                 if (computerSelection == 'rock'){
                     playerCount++;
-                    return "Paper beats rock, you Won"+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
+                    return "Paper beats rock, you Won"+'\n'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
                 }
                 else {
                     computerCount++;
-                    return 'You Lose'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
+                    return 'You Lose'+'\n'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
                 }
             }
             else if (playerSelection == 'scissors'){
                 if (computerSelection == 'paper'){
                     playerCount++;
-                    return "Scissor beats paper, you Won"+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
+                    return "Scissor beats paper, you Won"+'\n'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
                 }
                 else {
                     computerCount++;
-                    return 'You Lose'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
+                    return 'You Lose'+'\n'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
                 }
             }
             else if (playerSelection == 'rock') {
                 if (computerSelection == 'scissor'){
                     playerCount++;
-                    return "Rock beats scissor, you Won"+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
+                    return "Rock beats scissor, you Won"+'\n'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
                 }
                 else {
                     computerCount++;
-                    return 'You Lose'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
+                    return 'You Lose'+'\n'+(' Your Score: ' + playerCount)+(' Computer Score: ' + computerCount);
                 }
             }
             else {
                 alert ('Incorret answer');
             }
         }    
+        console.log (playround(playerSelection, computerSelection));
     }    
-    console.log (playround(playerSelection, computerSelection));
 }
 console.log (game());
 
