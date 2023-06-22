2a07033da1b81b5e3eafb20c08bad77333339810
hyperhkm <lucasjamaico@gmail.com>
2023-06-22 12:12:51 -0300

    .

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
-'use strict'
-const shapeSelection = ['rock', 'paper', 'scissor'];
-let playerSelection;
-function getComputerChoice (shapeSelection) {
-    return shapeSelection [Math.floor(Math.random()*shapeSelection.length)];
+const computerChoice = function () {
+    let randomNumber = Math.floor(Math.random()*3);
+    switch (randomNumber) {
+        case 0: return 'rock';
+        case 1: return 'paper';
+        case 2: return 'scissor';        
+    }
 }
-function game(){
-    for (let playerCount = 0, computerCount = 0; playerCount < 5 && computerCount < 5;){  
-        const computerSelection = getComputerChoice(shapeSelection);
-        function playround (playerSelection, computerSelection) {  
-            playerSelection = prompt ("Rock, Paper, Scissor: Choose your Weapon", ).toLowerCase();
-            console.log ("Your score: " + playerCount + " Computer score: " + computerCount);
-            if (playerSelection == computerSelection) {
-                return "Draw";
-            }
-            else if (playerSelection == 'paper' && computerSelection == 'rock') {
+
+function game () {
+    let playerCount = 0, computerCount = 0;
+
+    function playGame() {
+        const score = document.querySelector('#score');
+        const btnPaper = document.querySelector('button[value="paper"]')
+        const btnScissor = document.querySelector('button[value="scissor"]')
+        const btnRock = document.querySelector('button[value="rock"]')
+        const playerOptions = [btnPaper, btnRock, btnScissor];
+        
+        playerOptions.forEach (option => {
+            option.addEventListener('click', function() {
+                winner (this.innerText.toLowerCase(), computerChoice());
+                if (playerCount == 5) {playerCount = 0, computerCount = 0; return score.textContent = 'You are the winner'};
+                if (computerCount == 5) {playerCount = 0, computerCount = 0; return score.textContent = 'You lost the game'};
+            })
+        })
+    }
+
+    const winner = function (player, computer) {
+        const scoreTable = `Your score: ${playerCount} Computer score: ${computerCount}`;
+        switch (true) {
+            case player == computer: 
+                score.textContent = "It's a draw\n"+scoreTable;
+                break;
+            case player == 'paper' && computer == 'rock':
+                playerCount++;    
+                score.textContent = 'You won, paper beats rock!\n'+scoreTable;
+                break;
+            case player == 'rock' && computer == 'scissor':
                 playerCount++;
-                return "Paper beats rock, you Won";
-            }
-            else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-                playerCount++;
-                return "Scissor beats paper, you Won";
-            }
-            else if (playerSelection == 'rock' && computerSelection == 'scissor') {
-                playerCount++;
-                return "Rock beats scissor, you Won";
-            }
-            else {
-                if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissor') {
-                    computerCount++
-                    return console.log ('You lost'); 
-                }
-                else {
-                    return alert ("Incorrect, type again");
-                }    
-            }
-        }    
-        console.log (playround(playerSelection, computerSelection));
-    } 
+                score.textContent = 'You won, rock beats scissor\n'+scoreTable;
+                break;
+            case player == 'scissor' && computer == 'paper':
+                playerCount++;    
+                score.textContent = 'You won, scissor beats paper\n'+scoreTable;
+                break;
+            default: 
+                computerCount++;
+                score.textContent = 'You lost the round\n'+scoreTable;
+                break;        
+        }
+    }
+    playGame();
 }
-console.log (game());
+game();
